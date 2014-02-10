e8d6ec4509cf6ceba03faed70d76e859948efa44
nma <cloakedcyril@gmail.com>
2014-02-10 03:22:43 -0500

    majority progress.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ $( document ).ready(function() {
 
     $('#headermenuicon span.closemenu').hide();
 
+    console.log($('#gmap'));
+    if ($('#gmap').is(':visible')) {
+        console.log("Im in");
+        new Maplace({
+            locations: [{
+                lat: 43.47,
+                lon: -80.54,
+                title: 'University of Waterloo',
+                generate_controls: false, 
+                icon: 'https://uwaterloo.ca/brand-guidelines/sites/ca.brand-guidelines/files/resize/uploads/images/universityofwaterloo_logo_horiz_rgb_0-300x120.png', 
+                zoom: 12
+            }]
+        }).Load();
+    }
+
     if ($('#headermenuicon').css('visibility') === 'visible') {
         if ($('#headermenuicon span.openmenu').is(':visible') ) {
             $headermenu.hide();
